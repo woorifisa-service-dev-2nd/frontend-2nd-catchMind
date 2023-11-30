b9da4c351ad005c9fffa95b76913cd3b7ba0ade7
rlaxoqkf <rlaxoqkf96@gmail.com>
2023-11-30 10:38:27 +0900

    img 생성 코드중간 저장

--- public/app.js
+++ public/app.js
@@ -1,3 +1,33 @@
+/* eslint-disable no-unused-vars */
 /**
- * 기능 구현
+ * 이미지 생성 기능 구현
  */
+const inputText = "A cute cat";
+const genButton = "";
+const imgSrc = document.getElementById("myImage");
+/**
+ * 이미지 생성 버튼을 눌렀을 때 이미지 생성 api를 호출하도록
+ */
+genButton.addEventListener("click", ()=>{
+    
+});
+
+const imgGen = (text)=>{
+	const body = JSON.stringify({ prompt: text });
+
+	const url = "/generate";
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ query: text }),
+	};
+
+	fetch(url, options)
+		.then((response) => response.json())
+		.then((data) => {
+			imgSrc.imgSrc = data.imgaes.imgae;
+		})
+		.catch((error) => console.error(error));
+};
--- server.js
+++ server.js
@@ -1,15 +1,47 @@
+/* eslint-disable no-unused-vars */
 /**
  * api 연결 및 서버 설정
  */
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
+const REST_API_KEY = "f4daf48df7ebe7f67aa7af07a888179f";
+
+// localhost:3000/detectLangs로 요청 시 동작할 핸들러
+app.post("/generate", (req, res) => {
+	const request = require("request");
+
+	const text = req.body.prompt;
+
+	const url = "https://api.kakaobrain.com/v2/inference/karlo/t2i" ;
+
+	console.log("Received data from the client:", req.body);
+	// res.status(200).send('Data received successfully');
+
+	const options = {
+		url, // url: url과 같음
+		form: { "prompt": text},
+		headers: { "Authorization": `KakaoAK ${REST_API_KEY}`,
+			"Content-Type": "application/json" }
+	};
+	// 실제로 POST 요청 전송 부분
+	request.post(options, (error, response, body) => {
+
+		if (!error && response.statusCode == 200) {
+			res.send(body);
+		} else {
+			res.status(response.statusCode).end();
+			console.log("error = " + response.statusCode);
+		}
+	});
+});
+
 //index.html을 기본화면으로 세팅
-app.get('/', (req, res) => res.sendFile('index.html'));
+app.get("/", (req, res) => res.sendFile("index.html"));
 
 // 서버 인스턴스를 3000번 포트에서 대기하도록 명시
 const port = 3000;
