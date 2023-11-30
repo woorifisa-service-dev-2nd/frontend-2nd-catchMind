a2a4089384c7a05cacbd781b2ac91e663c90717f
HyeJin <hjhong9801@naver.com>
2023-11-30 10:36:34 +0900

    기초 html 세팅

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
 /**
  * 기능 구현
  */
+const text = document.getElementById('text');
+const size = document.getElementById('size')
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+document.querySelector('#ex-in').addEventListener('input',e=>{
+    document.querySelector('#ex-out').innerHTML= e.target.value;
+});
